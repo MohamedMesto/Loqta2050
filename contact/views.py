f2f0ecff71abfe63954850f99cf5f98aa68a2fa8
MohamedMesto <mohamedmesto111@gmail.com>
2024-09-10 23:56:53 +0200

    initial text to the apps views

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+ 
+
+def contact_me(request):
+    return HttpResponse("Questions or comments? Get in touch and we'll be happy to help.")

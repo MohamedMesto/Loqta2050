1858633735a5e09c674a0d91a4d875aa5d220d68
MohamedMesto <mohamedmesto111@gmail.com>
2024-09-11 09:25:49 +0200

    Deployment setting - change S - K

--- loqta2050_prj/settings.py
+++ loqta2050_prj/settings.py
@@ -28,9 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u4a2((m=e@x@2ack-4km8&u5i1lxn_ov+n8ys0cb5c3hrrpgda'
+# SECRET_KEY = 'django-insecure-u4a2((m=e@x@2ack-4km8&u5i1lxn_ov+n8ys0cb5c3hrrpgda'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 
 # DEBUG = True
 
